5b63956cd7db923bb4c7e6243f2790d2db8aceec
Fatur08 <dhutafaturrahman@gmail.com>
2025-11-28 10:36:19 +0700

    update

--- resources/views/TambahModalDLX.blade.php
+++ resources/views/TambahModalDLX.blade.php
@@ -62,6 +62,7 @@
         <div class="col-12">
             <div class="input-icon mb-3">
                 <span class="input-icon-addon">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
                 </span>
                 <input type="text" id="check_in_tampil" class="form-control flatpickr" placeholder="Masukkan Tanggal Check-In" autocomplete="off">
                 <input type="hidden" id="check_in" name="check_in">
@@ -77,6 +78,7 @@
         <div class="col-12">
             <div class="input-icon mb-3">
                 <span class="input-icon-addon">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
                 </span>
                 <input type="text" id="check_out_tampil" class="form-control flatpickr" placeholder="Masukkan Tanggal Check-Out" autocomplete="off">
                 <input type="hidden" id="check_out" name="check_out">
