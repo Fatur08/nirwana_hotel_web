0cdccfff84f0e77b06bf94204458549073beca82
Fatur08 <dhutafaturrahman@gmail.com>
2025-11-29 16:02:02 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -460,11 +460,11 @@
 
 
   <!-- Modal Informasi Kamar Standar (STD) -->
-  <div class="modal fade" id="modalinfo-STD" tabindex="-1" aria-labelledby="ModalDLXLabel" aria-hidden="true">
+  <div class="modal fade" id="modalinfo-STD" tabindex="-1" aria-labelledby="ModalSTDLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
-        <div class="modal-header bg-danger text-white">
-          <h5 class="modal-title" id="ModalDLXLabel">Informasi Kamar - Tipe Deluxe</h5>
+        <div class="modal-header bg-warning text-white">
+          <h5 class="modal-title" id="ModalSTDLabel">Informasi Kamar - Tipe Standar</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
         <div class="modal-body" id="loadModalSTD">
