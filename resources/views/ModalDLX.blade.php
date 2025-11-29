ee1528ec6b290ff79dd879f3c81a2691271249cc
Fatur08 <dhutafaturrahman@gmail.com>
2025-11-29 14:00:02 +0700

    update

--- resources/views/ModalDLX.blade.php
+++ resources/views/ModalDLX.blade.php
@@ -1,55 +1,46 @@
-<form action="#" method="POST" id="frmModalDLX" enctype="multipart/form-data">
-    @csrf
-    <input type="text" readonly value="{{ $nomor_kamar }}" id="nomor_kamar" class="form-control" name="nomor_kamar" placeholder="nomor_kamar" hidden>
-    <input type="text" readonly value="{{ $tipe_kamar }}" id="tipe_kamar" class="form-control" name="tipe_kamar" placeholder="tipe_kamar" hidden>
-    @php
-        if ($tipe_kamar == 1) {
-            $labelKamar = 'DLX' . $histori_kamar->nomor_kamar;
-        } elseif ($tipe_kamar == 2) {
-            $labelKamar = 'SPR' . $histori_kamar->nomor_kamar;
-        } else {
-            $labelKamar = 'STD' . $histori_kamar->nomor_kamar;
-        }
-    @endphp
+@php
+    if ($tipe_kamar == 1) {
+        $labelKamar = 'DLX' . $histori_kamar->nomor_kamar;
+    } elseif ($tipe_kamar == 2) {
+        $labelKamar = 'SPR' . $histori_kamar->nomor_kamar;
+    } else {
+        $labelKamar = 'STD' . $histori_kamar->nomor_kamar;
+    }
+@endphp
 
-    <table class="table table-bordered">
-        <tr>
-            <td width="40%"><b>Nomor Kamar</b></td>
-            <td width="60%">{{ $labelKamar }}</td>
-        </tr>
-
-        <tr>
-            <td><b>Nama Tamu</b></td>
-            <td>{{ $histori_kamar->nama_tamu ?? '-' }}</td>
-        </tr>
-
-        <tr>
-            <td><b>Nomor KTP Tamu</b></td>
-            <td>{{ $histori_kamar->nomor_ktp_tamu ?? '-' }}</td>
-        </tr>
-
-        <tr>
-            <td><b>Check In</b></td>
-            <td>
-                {{ $histori_kamar->check_in
-                    ? \Carbon\Carbon::parse($histori_kamar->check_in)->translatedFormat('l, d F Y')
-                    : '-' 
-                }}
-            </td>
-        </tr>
-            
-        <tr>
-            <td><b>Check Out</b></td>
-            <td>
-                {{ $histori_kamar->check_out
-                    ? \Carbon\Carbon::parse($histori_kamar->check_out)->translatedFormat('l, d F Y')
-                    : '-' 
-                }}
-            </td>
-        </tr>
-    </table>
-
-    <button class="btn btn-danger w-100 mt-3" type="submit">
-        Simpan
-    </button>
-</form>
+<table class="table table-bordered">
+    <tr>
+        <td width="40%"><b>Nomor Kamar</b></td>
+        <td width="60%">{{ $labelKamar }}</td>
+    </tr>
+    <tr>
+        <td><b>Nama Tamu</b></td>
+        <td>{{ $histori_kamar->nama_tamu ?? '-' }}</td>
+    </tr>
+    <tr>
+        <td><b>Nomor KTP Tamu</b></td>
+        <td>{{ $histori_kamar->nomor_ktp_tamu ?? '-' }}</td>
+    </tr>
+    <tr>
+        <td><b>Check In</b></td>
+        <td>
+            {{ $histori_kamar->check_in
+                ? \Carbon\Carbon::parse($histori_kamar->check_in)->translatedFormat('l, d F Y')
+                : '-' 
+            }}
+        </td>
+    </tr>
+        
+    <tr>
+        <td><b>Check Out</b></td>
+        <td>
+            {{ $histori_kamar->check_out
+                ? \Carbon\Carbon::parse($histori_kamar->check_out)->translatedFormat('l, d F Y')
+                : '-' 
+            }}
+        </td>
+    </tr>
+</table>
+<a href="{{ route('/') }}" class="btn btn-danger w-100 mt-3">
+    Kembali
+</a>
