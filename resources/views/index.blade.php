58effd54b9781a61b739e65330568a29105cd970
Fatur08 <dhutafaturrahman@gmail.com>
2025-12-01 14:47:03 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -140,26 +140,46 @@
     <div class="kotak-cari">
       <h1>Cari Tanggal</h1>
       <form action="/" method="GET" id="frmCariTanggal" enctype="multipart/form-data">
-        @csrf
-        <div class="row">
-          <div class="col-8">
-            <div class="input-icon mb-3">
-                <span class="input-icon-addon">
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M16 3l0 4" /><path d="M8 3l0 4" /><path d="M4 11l16 0" /><path d="M8 15h2v2h-2z" /></svg>
-                </span>
-                <!-- INPUT TAMPILAN (UNTUK USER) -->
-                <input type="text" id="tgl_tampil" class="form-control flatpickr" placeholder="Masukkan Tanggal" autocomplete="off">
-                 
-                <!-- INPUT ASLI UNTUK DATABASE -->
-                <input type="hidden" id="cari_tanggal" name="cari_tanggal">
-            </div>
+          @csrf
+          <div class="row">
+              <div class="col-8">
+                  <div class="input-icon mb-3">
+                      <span class="input-icon-addon">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                               viewBox="0 0 24 24" fill="none" stroke="currentColor" 
+                               stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                               class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-event">
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                              <path d="M4 5m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+                              <path d="M16 3l0 4" />
+                              <path d="M8 3l0 4" />
+                              <path d="M4 11l16 0" />
+                              <path d="M8 15h2v2h-2z" />
+                          </svg>
+                      </span>
+      
+                      <!-- ✅ INPUT TAMPILAN (FONT 16pt) -->
+                      <input type="text" 
+                             id="tgl_tampil" 
+                             class="form-control flatpickr"
+                             placeholder="Masukkan Tanggal"
+                             autocomplete="off"
+                             style="font-size:16pt; padding:10px;">
+                       
+                      <!-- INPUT ASLI UNTUK DATABASE -->
+                      <input type="hidden" id="cari_tanggal" name="cari_tanggal">
+                  </div>
+              </div>
+      
+              <div class="col-4">
+                  <!-- ✅ TOMBOL CARI (FONT 16pt) -->
+                  <button class="btn btn-success w-100" 
+                          type="submit"
+                          style="font-size:16pt; padding:10px;">
+                      Cari
+                  </button>
+              </div>
           </div>
-          <div class="col-4">
-            <button class="btn btn-success w-100" type="submit">
-              Cari
-            </button>
-          </div>
-        </div>
       </form>
     </div>
     
