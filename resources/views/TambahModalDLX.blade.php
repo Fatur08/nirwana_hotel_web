450c20c70fbdf2965844fa8d8a13e39f16bf15b8
Fatur08 <dhutafaturrahman@gmail.com>
2026-03-27 16:54:13 +0700

    update

--- resources/views/TambahModalDLX.blade.php
+++ resources/views/TambahModalDLX.blade.php
@@ -127,6 +127,22 @@
                   <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                   <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-currency-dollar"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" /><path d="M12 3v3m0 12v3" /></svg>
                 </span>
+                <input type="number" value="" id="request" class="form-control" style="font-size:16pt;" name="request" placeholder="Masukkan Request">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <h5 style="font-size:16pt;">Biaya Tambahan (Opsional)</h5>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="input-icon mb-3">
+                <span class="input-icon-addon">
+                  <!-- Download SVG icon from http://tabler-icons.io/i/user -->
+                  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-currency-dollar"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" /><path d="M12 3v3m0 12v3" /></svg>
+                </span>
                 <input type="number" value="" id="biaya_tambahan" class="form-control" style="font-size:16pt;" name="biaya_tambahan" placeholder="Masukkan Biaya Tambahan">
             </div>
         </div>
