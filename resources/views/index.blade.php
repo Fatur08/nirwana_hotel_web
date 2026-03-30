3de46831c2e075ced10bde081c551bd6b1f37d42
Fatur08 <dhutafaturrahman@gmail.com>
2026-03-30 13:48:49 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -289,29 +289,27 @@
     </div>
 
 
-    <div>
-        <div class="container-xl">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="table-wrapper">
-                            <div class="table-responsive">
-                                <table class="table custom-table">
-                                    <thead>
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Nama</th>
-                                            <th>Divisi</th>
-                                            <th>No. HP</th>
-                                            <th>Foto</th>
-                                            <th>KTP</th>
-                                            <th>Status</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
-                            </div>
+    <div class="container-xl">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="table-wrapper">
+                        <div class="table-responsive">
+                            <table class="table custom-table">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Nama</th>
+                                        <th>Divisi</th>
+                                        <th>No. HP</th>
+                                        <th>Foto</th>
+                                        <th>KTP</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
