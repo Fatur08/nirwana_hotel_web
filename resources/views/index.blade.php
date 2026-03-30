638fa4b3f6d6e4af30212c35b682db73e58974f5
Fatur08 <dhutafaturrahman@gmail.com>
2026-03-30 14:18:53 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -314,8 +314,8 @@
             <div class="col-3"><h1>Buat Foto</h1></div>
             <div class="col-6">
                 <h1>Kamar Deluxe</h1><br>
-                <h3>Tersedia 7 Kamar</h3>
-                <h3>Mulai dari</h3><h2>Rp.300.000</h2><h3> / malam</h3>
+                <p>Tersedia 7 Kamar</p><br>
+                <p>Mulai dari<strong>Rp.300.000</strong> / malam</p>
             </div>
             <div class="col-3">
                 <a href="#" class="TambahModalDLX btn btn-success mb-2 w-100" style="font-size:16pt;" tipe_kamar="1" data-tanggal="{{ $cari_tanggal }}">
