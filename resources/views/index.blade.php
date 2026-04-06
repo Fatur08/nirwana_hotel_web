6644107a296848da3076ad345bf8f8bbf770187b
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-06 21:28:48 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -325,19 +325,36 @@
                         <td>Booking</td>
                         <td>06 April 2026</td>
                         <td>07 April 2026</td>
-                        <td>
-                            <a href="#"
-                               class="TambahModalDLX btn btn-success"
-                               tipe_kamar="1"
-                               data-tanggal="{{ $cari_tanggal }}">
-                               <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-printer"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" /><path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" /><path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" /></svg>
-                            </a>
-                            <a href="#"
-                               class="TambahModalDLX btn btn-info"
-                               tipe_kamar="1"
-                               data-tanggal="{{ $cari_tanggal }}">
-                               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M12 9h.01" /><path d="M11 12h1v4h1" /></svg>
-                            </a>
+                        <td class="text-center">
+                            <div class="d-flex justify-content-center align-items-center gap-2">
+                                <a href="#"
+                                   class="TambahModalDLX btn btn-success"
+                                   tipe_kamar="1"
+                                   data-tanggal="{{ $cari_tanggal }}">
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        class="icon icon-tabler icon-tabler-printer">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2"/>
+                                        <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4"/>
+                                        <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z"/>
+                                   </svg>
+                                </a>
+                        
+                                <a href="#"
+                                   class="TambahModalDLX btn btn-info"
+                                   tipe_kamar="1"
+                                   data-tanggal="{{ $cari_tanggal }}">
+                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        class="icon icon-tabler icon-tabler-info-circle">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"/>
+                                        <path d="M12 9h.01"/>
+                                        <path d="M11 12h1v4h1"/>
+                                   </svg>
+                                </a>
+                            </div>
                         </td>
                     </tr>
                 </tbody>
