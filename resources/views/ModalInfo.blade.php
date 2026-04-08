1e35e79a421e6d1677e1188a54d21d11c222a963
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-08 22:19:24 +0700

    update

--- resources/views/ModalInfo.blade.php
+++ resources/views/ModalInfo.blade.php
@@ -6,14 +6,21 @@
         <td><b>Nama Tamu</b></td>
     </tr>
     <tr>
-        <td><b>Nomor KTP Tamu</b></td>
-    </tr>
-    <tr>
         <td><b>Check In</b></td>
     </tr>
         
     <tr>
         <td><b>Check Out</b></td>
+    </tr>
+    <tr>
+        <td><b>Jumlah Kamar Dipesan</b></td>
+    </tr>
+        
+    <tr>
+        <td><b>Foto KTP</b></td>
+    </tr>
+    <tr>
+        <td><b>Request</b></td>
     </tr>
 </table>
 <button type="button" class="btn btn-secondary w-100 mt-3" data-bs-dismiss="modal">
