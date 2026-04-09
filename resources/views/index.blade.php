0d919a23439508c8af6c5756fd8c438b75cc43bb
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-09 23:18:52 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1748,7 +1748,18 @@
 
 
         function printResi() {
+
+            let printContents = document.getElementById('area-print').innerHTML;
+            let originalContents = document.body.innerHTML;
+
+            document.body.innerHTML = printContents;
+
             window.print();
+
+            document.body.innerHTML = originalContents;
+
+            location.reload();
+
         }
 
 
