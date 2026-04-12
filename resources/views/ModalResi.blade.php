7564511695bb223317c3783c6b03695afa55f0e1
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-12 09:11:56 +0700

    update

--- resources/views/ModalResi.blade.php
+++ resources/views/ModalResi.blade.php
@@ -1,59 +1,59 @@
 <div id="area-print">
-    <table class="table-garis">
-        <tbody>
+    <div class="container-fluid">
 
-            <tr>
-                <td style="font-size:26px; font-weight:bold; text-align:left;">
+        <!-- Judul + Logo -->
+        <div class="row align-items-center">
+            <div class="col-8">
+                <div style="font-size:26px; font-weight:bold;">
                     Bukti Pembayaran
-                </td>
-
-                <td rowspan="2" style="text-align:right;">
-                    <img src="{{ asset('assets/img/nirwana_hotel.png') }}" style="height:150px;">
-                </td>
-            </tr>
-
-            <tr>
-                <td style="font-size:20px; font-weight:bold; text-align:left;">
+                </div>
+                <div style="font-size:20px; font-weight:bold;">
                     Nirwana Hotel Kalianda ★
-                </td>
+                </div>
+            </div>
 
-            </tr>
+            <div class="col-4 text-end">
+                <img src="{{ asset('assets/img/nirwana_hotel.png') }}" style="height:100px;">
+            </div>
+        </div>
 
-            <tr>
-                <td style="font-size:16px; text-align:left;">
-                    Jl. Kolonel Makmun Rasyid No.18, Way Urang, Kec. Kalianda,
-                </td>
-                <td style="font-size:16px; text-align:right; font-weight:bold;">
-                    Dipesan Pada 09 April 2026
-                </td>
-            </tr>
+        <!-- Alamat + Tanggal -->
+        <div class="row mt-2">
+            <div class="col-7" style="font-size:16px;">
+                Jl. Kolonel Makmun Rasyid No.18, Way Urang, Kec. Kalianda,
+            </div>
 
-            <tr>
-                <td style="font-size:16px; text-align:left;">
-                    Kabupaten Lampung Selatan, Lampung 35551
-                </td>
-            </tr>
+            <div class="col-5 text-end" style="font-size:16px; font-weight:bold;">
+                Dipesan Pada 09 April 2026
+            </div>
+        </div>
 
-            <tr>
-                <td style="font-size:16px; text-align:left;">
-                    0851 5609 1313
-                </td>
-            </tr>
+        <!-- Alamat lanjut -->
+        <div class="row">
+            <div class="col-12" style="font-size:16px;">
+                Kabupaten Lampung Selatan, Lampung 35551
+            </div>
+        </div>
 
-            <tr>
-                <td style="font-size:5px; font-weight:bold; text-align:left;">.</td>
-            </tr>
+        <!-- Telepon -->
+        <div class="row">
+            <div class="col-12" style="font-size:16px;">
+                0851 5609 1313
+            </div>
+        </div>
 
-            <tr>
-                <td style="font-size:5px; font-weight:bold; text-align:left;">.</td>
-            </tr>
+        <!-- Spacer -->
+        <div class="row mt-3"></div>
+        <div class="row mt-3"></div>
 
-            <tr>
-                <td style="font-size:26px; font-weight:bold; text-align:left;">Detail Pemesanan</td>
-            </tr>
+        <!-- Judul Detail -->
+        <div class="row">
+            <div class="col-12" style="font-size:26px; font-weight:bold;">
+                Detail Pemesanan
+            </div>
+        </div>
 
-        </tbody>
-    </table>
+    </div>
 
 
 
