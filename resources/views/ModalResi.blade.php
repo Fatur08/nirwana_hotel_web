844d83ef32940c7ae21959cf500cd7624f2b1c1c
Fatur08 <dhutafaturrahman@gmail.com>
2026-04-12 09:41:29 +0700

    update

--- resources/views/ModalResi.blade.php
+++ resources/views/ModalResi.blade.php
@@ -56,7 +56,7 @@
 
         <div class="row">
             <div class="col-12">
-                <table class="tabel-garis">
+                <table class="table-garis">
                     <tbody>
                         <tr>
                             <td>
