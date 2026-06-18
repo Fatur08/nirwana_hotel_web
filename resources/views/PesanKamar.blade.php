3a2e6e3d028a7d1118e9e414c83c7345834a832d
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-18 11:38:42 +0700

    up

--- resources/views/PesanKamar.blade.php
+++ resources/views/PesanKamar.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="input-icon mb-7">
+            <div class="input-icon mb-6">
                 <span class="input-icon-addon">
                     <!-- Download SVG icon from http://tabler-icons.io/i/user -->
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -31,7 +31,7 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="input-icon mb-7">
+            <div class="input-icon mb-6">
                 <span class="input-icon-addon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -58,7 +58,7 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="input-icon mb-7">
+            <div class="input-icon mb-6">
                 <span class="input-icon-addon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
