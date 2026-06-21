212134345636fd812f205e3f2175f93fe2d561d5
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-21 11:10:58 +0700

    up

--- resources/views/KetersediaanKamar.blade.php
+++ resources/views/KetersediaanKamar.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.index')
 @section('content')
-    <div class="row mb-3">
-        <div class="col-12">
-            <a href="{{ url('/') }}" class="btn btn-secondary w-100" style="font-size:16pt;">
+    <div class="mb-3">
+        <a href="{{ url('/') }}" class="btn btn-dark">
 
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2">
 
-                    <path d="M15 6l-6 6l6 6" />
-                </svg>
+                <path d="M15 6l-6 6l6 6" />
+            </svg>
 
-                Kembali
-            </a>
-        </div>
+            Kembali
+        </a>
     </div>
 @endsection
 @push('myscript')
