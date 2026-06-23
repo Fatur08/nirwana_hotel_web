30daded54fd9416d3d0c7b1f481e99d2f3a864dc
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-23 20:41:34 +0700

    up

--- resources/views/InformasiPemesanan.blade.php
+++ resources/views/InformasiPemesanan.blade.php
@@ -287,7 +287,7 @@
 
     <!-- Modal Informasi Pemesanan -->
     <div class="modal fade" id="modal-info" tabindex="-1" aria-labelledby="ModalInfoLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog modal-dialog-centered" style="max-width:900px;">
             <div class="modal-content">
                 <div class="modal-header bg-secondary text-white">
                     <h5 class="modal-title" id="ModalInfoLabel" style="font-size:16pt;">Informasi Pemesanan</h5>
@@ -467,22 +467,22 @@
 
             frameDoc.open();
             frameDoc.write(`
-                                            <html>
-                                            <head>
-                                                <title>Print Resi</title>
-                                                <style>
-                                                    body{
-                                                        font-family: Arial;
-                                                        font-size:14px;
-                                                        padding:20px;
-                                                    }
-                                                </style>
-                                            </head>
-                                            <body>
-                                                ${isi}
-                                            </body>
-                                            </html>
-                                        `);
+                                                <html>
+                                                <head>
+                                                    <title>Print Resi</title>
+                                                    <style>
+                                                        body{
+                                                            font-family: Arial;
+                                                            font-size:14px;
+                                                            padding:20px;
+                                                        }
+                                                    </style>
+                                                </head>
+                                                <body>
+                                                    ${isi}
+                                                </body>
+                                                </html>
+                                            `);
             frameDoc.close();
 
             frame.contentWindow.focus();
