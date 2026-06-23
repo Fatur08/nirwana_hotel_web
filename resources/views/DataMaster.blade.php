3473125b022bb2215bb5b864b64ab056ef56d145
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-23 11:57:28 +0700

    up

--- resources/views/DataMaster.blade.php
+++ resources/views/DataMaster.blade.php
@@ -153,35 +153,5 @@
 
             return 'Rp.' + parseInt(angka).toLocaleString('id-ID');
         }
-
-        $('.rupiah').on('input', function () {
-
-            $(this).val(
-                formatRupiah($(this).val())
-            );
-
-        });
-
-        $('.rupiah').on('focus', function () {
-
-            let angka = $(this).val().replace(/[^0-9]/g, '');
-
-            $(this).val(angka);
-
-        });
-
-        $('.rupiah').on('blur', function () {
-
-            let angka = $(this).val();
-
-            if (angka !== '') {
-
-                $(this).val(
-                    'Rp.' + parseInt(angka).toLocaleString('id-ID')
-                );
-
-            }
-
-        });
     </script>
 @endpush
