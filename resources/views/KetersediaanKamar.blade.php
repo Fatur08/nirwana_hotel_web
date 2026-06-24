7c6af6e4fd828e554d354bdad279fbfd19491fbf
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-25 00:44:23 +0700

    up

--- resources/views/KetersediaanKamar.blade.php
+++ resources/views/KetersediaanKamar.blade.php
@@ -137,30 +137,29 @@
                 <label class="form-label fw-bold">
                     Bulan
                 </label>
+                @php
+                    $bulanList = [
+                        1 => 'Januari',
+                        2 => 'Februari',
+                        3 => 'Maret',
+                        4 => 'April',
+                        5 => 'Mei',
+                        6 => 'Juni',
+                        7 => 'Juli',
+                        8 => 'Agustus',
+                        9 => 'September',
+                        10 => 'Oktober',
+                        11 => 'November',
+                        12 => 'Desember'
+                    ];
+                @endphp
                 <select name="bulan" class="form-control">
-                    @php
-                        $bulanList = [
-                            1 => 'Januari',
-                            2 => 'Februari',
-                            3 => 'Maret',
-                            4 => 'April',
-                            5 => 'Mei',
-                            6 => 'Juni',
-                            7 => 'Juli',
-                            8 => 'Agustus',
-                            9 => 'September',
-                            10 => 'Oktober',
-                            11 => 'November',
-                            12 => 'Desember'
-                        ];
-                    @endphp
-                    <select name="bulan" class="form-control">
-                        @foreach($bulanList as $key => $nama)
-                            <option value="{{ $key }}" {{ $bulan == $key ? 'selected' : '' }}>
-                                {{ $nama }}
-                            </option>
-                        @endforeach
-                    </select>
+                    @foreach($bulanList as $key => $nama)
+                        <option value="{{ $key }}" {{ $bulan == $key ? 'selected' : '' }}>
+                            {{ $nama }}
+                        </option>
+                    @endforeach
+                </select>
             </div>
         </div>
 
