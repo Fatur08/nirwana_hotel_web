d2eb80f0d1edfdb1b785c10f7ce93fc808529db5
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-26 01:18:47 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.index')
 @section('content')
     <style>
+        html,
         body {
             background: linear-gradient(to bottom, #e8dbb9, #eaf3ff);
             font-family: 'Be Vietnam Pro', sans-serif;
             margin: 0;
             padding: 15px;
             min-height: 100vh;
+            min-width: 1366px;
             max-width: 100%;
             width: 100%;
             font-size: 20px;
+            overflow-x: auto;
         }
 
 
@@ -951,10 +954,10 @@
                         $('#check_out').val('');
 
                         $('#jumlah_kamar_dipesan').html(`
-                                                                                                                                                                    <option value="">
-                                                                                                                                                                        -- Pilih Tanggal Check Out Dulu --
-                                                                                                                                                                    </option>
-                                                                                                                                                                `);
+                                                                                                                                                                            <option value="">
+                                                                                                                                                                                -- Pilih Tanggal Check Out Dulu --
+                                                                                                                                                                            </option>
+                                                                                                                                                                        `);
 
                         $('#kamar_tersedia_title').hide();
                         $('#kamar_tersedia_list').hide();
@@ -989,10 +992,10 @@
                             for (let i = 1; i <= totalKamar; i++) {
 
                                 opsiJumlah += `
-                                                                                                                                                                                            <option value="${i}">
-                                                                                                                                                                                                ${i} Kamar
-                                                                                                                                                                                            </option>
-                                                                                                                                                                                        `;
+                                                                                                                                                                                                    <option value="${i}">
+                                                                                                                                                                                                        ${i} Kamar
+                                                                                                                                                                                                    </option>
+                                                                                                                                                                                                `;
                             }
 
                             $('#jumlah_kamar_dipesan').html(opsiJumlah);
@@ -1008,10 +1011,10 @@
                 $('#check_out').val('');
 
                 $('#jumlah_kamar_dipesan').html(`
-                                                                                                                                                                            <option value="">
-                                                                                                                                                                                -- Pilih Tanggal Check In Dulu --
-                                                                                                                                                                            </option>
-                                                                                                                                                                        `);
+                                                                                                                                                                                    <option value="">
+                                                                                                                                                                                        -- Pilih Tanggal Check In Dulu --
+                                                                                                                                                                                    </option>
+                                                                                                                                                                                `);
 
                 $('#kamar_tersedia_title').hide();
                 $('#kamar_tersedia_list').hide();
