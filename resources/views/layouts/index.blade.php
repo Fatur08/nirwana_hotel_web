c8b71ac64b7051d1459cd11b90a3d6a525de27be
Fatur08 <dhutafaturrahman@gmail.com>
2026-06-26 01:31:13 +0700

    up

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -14,7 +14,7 @@
 <head>
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=1000">
+  <meta name="viewport" content="width=1080">
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Home Page - Hotel Nirwana</title>
   <script defer data-api="/stats/api/event" data-domain="preview.tabler.io" src="/stats/js/script.js"></script>
