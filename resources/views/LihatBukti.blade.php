e0719065757e7a64956c83b07e1c0f9111dd0450
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-02 22:50:16 +0700

    up

--- resources/views/LihatBukti.blade.php
+++ resources/views/LihatBukti.blade.php
@@ -63,9 +63,38 @@
 
         @else
 
-            <div class="alert alert-danger">
-                Bukti pembayaran belum tersedia.
-            </div>
+            @if($bukti && $bukti->status_pembayaran == 1 && $bukti->metode_pembayaran == 'Cash')
+
+                <div class="alert alert-success text-center p-4">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 24 24" fill="none"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mb-3">
+
+                        <path d="M5 12l5 5l10 -10" />
+
+                    </svg>
+
+                    <h3 class="mb-2">Sudah Dibayar Cash</h3>
+
+                    <p class="mb-0">
+                        Pembayaran dilakukan secara tunai sehingga tidak memiliki bukti transfer.
+                    </p>
+
+                </div>
+
+            @elseif($bukti && $bukti->status_pembayaran == 1)
+
+                <div class="alert alert-warning text-center">
+                    Bukti pembayaran tidak tersedia.
+                </div>
+
+            @else
+
+                <div class="alert alert-danger text-center">
+                    Bukti pembayaran belum tersedia.
+                </div>
+
+            @endif
 
         @endif
 
