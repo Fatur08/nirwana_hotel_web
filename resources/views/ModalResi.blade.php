fe1ea81eb3fa23d1fd245c546b9c77f0a08901f5
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-03 23:16:09 +0700

    up

--- resources/views/ModalResi.blade.php
+++ resources/views/ModalResi.blade.php
@@ -72,7 +72,7 @@
             <tr>
                 <td>Check - In</td>
                 <td style="text-align:center;">:</td>
-                <td>{{ $checkIn->translatedFormat('d F Y') }}</td>
+                <td style="width:150px;">{{ $checkIn->translatedFormat('d F Y') }}</td>
 
                 <td colspan="2" style="width:180px; text-align:left;">Check - Out :
                     {{ $checkOut->translatedFormat('d F Y') }}
