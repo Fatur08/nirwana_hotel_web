019326a930af825d6db24d79133671bc83ebc85d
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-03 23:36:06 +0700

    up

--- resources/views/ModalResi.blade.php
+++ resources/views/ModalResi.blade.php
@@ -73,9 +73,10 @@
                 <td>Check - In</td>
                 <td style="text-align:center;">:</td>
                 {{-- <td style="width:190px;">{{ $checkIn->translatedFormat('d F Y') }}</td> --}}
-                <td style="width:190px;">30 Desember 2026</td>
+                <td style="width:300px;">30 Desember 2026</td>
 
-                <td colspan="2" style="width:160px; text-align:left;">Check - Out :
+                <td colspan="2" style="width:160px; text-align:left;">
+                    Check - Out&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;
                     {{ $checkOut->translatedFormat('d F Y') }}
                 </td>
             </tr>
