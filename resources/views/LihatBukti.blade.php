81a5c9f053b468d2e26ef84b1c36812b78545ec9
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-22 02:30:33 +0700

    up

--- resources/views/LihatBukti.blade.php
+++ resources/views/LihatBukti.blade.php
@@ -49,6 +49,27 @@
 
                     <hr>
 
+
+                    @if($bukti->total_dp > 0)
+                        <div class="alert alert-warning">
+                            <h5 class="mb-3">
+                                Riwayat Pembayaran DP
+                            </h5>
+
+                            <hr>
+
+                            <p class="mb-2">
+                                <strong>Metode DP :</strong>
+                                {{ $bukti->metode_dp ?? '-' }}
+                            </p>
+
+                            <p class="mb-0">
+                                <strong>Nominal DP :</strong>
+                                Rp {{ number_format($bukti->total_dp, 0, ',', '.') }}
+                            </p>
+                        </div>
+                    @endif
+
                     @if($bukti->metode_dp == 'Cash')
                         <div class="alert alert-success">
                             <h5 class="mb-3">
