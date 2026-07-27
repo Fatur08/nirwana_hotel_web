93414094f8d71946cf8119c96b82eb7d0c3eb7da
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-27 10:44:48 +0700

    up

--- resources/views/layouts/tentang-kami.blade.php
+++ resources/views/layouts/tentang-kami.blade.php
@@ -2,10 +2,13 @@
 <html lang="en">
 
 <head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=980">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
   <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
   <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/id.js"></script>
+  <link href="{{ asset('tabler/dist/css/tabler.min.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -17,6 +20,7 @@
   <!-- Tabler Core -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
   @stack('myscript')
+  <script src="{{ asset('tabler/dist/js/tabler.min.js') }}"></script>
 </body>
 
 </html>
