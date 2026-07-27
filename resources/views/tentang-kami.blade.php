b55735135d195a03aa1d30462778529ae6c8bed9
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-27 13:09:59 +0700

    up

--- resources/views/tentang-kami.blade.php
+++ resources/views/tentang-kami.blade.php
@@ -221,34 +221,6 @@
             </div>
 
             <div class="card-body">
-
-                <h2 class="text-primary fw-bold mb-4">
-                    NIRWANA GUEST HOUSE
-                </h2>
-
-                {{-- Bidang Usaha --}}
-                <div class="info-item d-flex mb-4">
-
-                    <div class="info-icon me-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2">
-
-                            <path d="M3 21h18" />
-                            <path d="M5 21V7l8-4v18" />
-                            <path d="M19 21V11l-6-4" />
-                        </svg>
-                    </div>
-
-                    <div>
-                        <h5>Bidang Usaha</h5>
-
-                        <p class="mb-0">
-                            Penginapan dan Restoran
-                        </p>
-                    </div>
-
-                </div>
-
                 {{-- Alamat --}}
                 <div class="info-item d-flex mb-4">
 
