7b5255c935846616c7ab46f6a33e11dcb3916375
Fatur08 <dhutafaturrahman@gmail.com>
2026-07-28 12:13:10 +0700

    up

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -278,18 +278,14 @@
 
 
 
-        {{-- ==========================================
-        BUTTON
-        ========================================== --}}
-        <div class="text-center">
-
-            <a href="{{ url('/') }}" class="btn btn-secondary btn-lg px-5">
-
-                ← Back
-
-            </a>
-
-        </div>
+        {{-- BUTTON KEMBALI --}}
+        <a href="{{ url('/') }}" class="btn btn-secondary w-100 mt-4" style="font-size:20pt;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2">
+                <path d="M15 6l-6 6l6 6" />
+            </svg>
+            Back to Login
+        </a>
 
     </div>
 
